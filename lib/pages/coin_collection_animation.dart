6ff938544c394fc0ace8d1bca003f9ef62a0b22c
Kerby <kerbycanaria@gmail.com>
2025-05-10 23:24:23 +0800

    malapit na

--- lib/pages/coin_collection_animation.dart
+++ lib/pages/coin_collection_animation.dart
@@ -1,13 +1,15 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-class CoinBouncePage extends StatefulWidget {
+class CoinCollectAnimation extends StatefulWidget {
   @override
-  _CoinBouncePageState createState() => _CoinBouncePageState();
+  _CoinCollectAnimationState createState() => _CoinCollectAnimationState();
 }
 
-class _CoinBouncePageState extends State<CoinBouncePage>
+class _CoinCollectAnimationState extends State<CoinCollectAnimation>
     with TickerProviderStateMixin {
   final int coinCount = 8;
+  bool showCoins = false;
 
   late List<AnimationController> bounceControllers;
   late List<Animation<double>> bounceAnimations;
@@ -15,58 +17,106 @@ class _CoinBouncePageState extends State<CoinBouncePage>
   late List<AnimationController> moveControllers;
   late List<Animation<Offset>> moveAnimations;
 
-  bool coinsVisible = false;
+  final GlobalKey containerKey = GlobalKey();
+
+  Offset containerBottomCenter = Offset.zero;
 
   @override
   void initState() {
     super.initState();
 
-    // Bounce animation setup
     bounceControllers = List.generate(
       coinCount,
-      (index) => AnimationController(
+      (_) => AnimationController(
         vsync: this,
-        duration: Duration(milliseconds: 600),
+        duration: Duration(milliseconds: 400),
       ),
     );
+
     bounceAnimations = bounceControllers.map((controller) {
       return Tween<double>(begin: 0.0, end: 1.0).animate(
         CurvedAnimation(parent: controller, curve: Curves.elasticOut),
       );
     }).toList();
 
-    // Movement animation setup
     moveControllers = List.generate(
       coinCount,
-      (index) => AnimationController(
+      (_) => AnimationController(
         vsync: this,
-        duration: Duration(milliseconds: 800),
+        duration: Duration(milliseconds: 900),
       ),
     );
+
     moveAnimations = moveControllers.map((controller) {
       return Tween<Offset>(
         begin: Offset.zero,
-        end: Offset(1.5, -3.0), // Moves to top-right
-      ).animate(CurvedAnimation(parent: controller, curve: Curves.easeIn));
+        end: Offset(3.0, -5.0), // Move top-right
+      ).animate(CurvedAnimation(parent: controller, curve: Curves.easeInOut));
     }).toList();
   }
 
-  void showAndCollectCoins() {
+  void triggerAnimation() {
+    final RenderBox box = containerKey.currentContext!.findRenderObject() as RenderBox;
+    final containerPosition = box.localToGlobal(Offset.zero);
+    final size = box.size;
+
     setState(() {
-      coinsVisible = true;
+      containerBottomCenter = Offset(
+        containerPosition.dx + size.width / 2 - 15, // center minus half coin
+        containerPosition.dy + size.height - 30,    // bottom minus coin height
+      );
+      showCoins = true;
     });
 
     for (int i = 0; i < coinCount; i++) {
-      Future.delayed(Duration(milliseconds: i * 100), () {
+      Future.delayed(Duration(milliseconds: i * 80), () {
         bounceControllers[i].forward();
-
-        // Start move animation after bounce
-        Future.delayed(Duration(milliseconds: 800), () {
+        Future.delayed(Duration(milliseconds: 500), () {
           moveControllers[i].forward();
         });
       });
     }
   }
+
+  Widget buildCoin(int index) {
+    return AnimatedBuilder(
+      animation: moveControllers[index],
+      builder: (_, __) {
+        return Positioned(
+          left: containerBottomCenter.dx,
+          top: containerBottomCenter.dy,
+          child: Transform.translate(
+            offset: moveAnimations[index].value * 200,
+            child: ScaleTransition(
+              scale: bounceAnimations[index],
+              child: Image.asset('assets/images/coin.png', width: 30, height: 30),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+//     Widget buildCoin(int index) {
+//   double spread = (index - coinCount / 2) * 20; // spread - e.g., -60 to +60
+
+//   return AnimatedBuilder(
+//     animation: moveControllers[index],
+//     builder: (_, __) {
+//       return Positioned(
+//         left: containerBottomCenter.dx + spread,
+//         top: containerBottomCenter.dy,
+//         child: Transform.translate(
+//           offset: moveAnimations[index].value * 200,
+//           child: ScaleTransition(
+//             scale: bounceAnimations[index],
+//             child: Image.asset('assets/images/coin.png', width: 30, height: 30),
+//           ),
+//         ),
+//       );
+//     },
+//   );
+// }
 
   @override
   void dispose() {
@@ -76,46 +126,26 @@ class _CoinBouncePageState extends State<CoinBouncePage>
     super.dispose();
   }
 
-  Widget buildCoin(int index) {
-    return SlideTransition(
-      position: moveAnimations[index],
-      child: ScaleTransition(
-        scale: bounceAnimations[index],
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Image.asset('assets/images/coin.png', width: 40, height: 40),
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Coin Collect Animation")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          ElevatedButton(
-            onPressed: showAndCollectCoins,
-            child: Text("Show & Collect Coins"),
-          ),
-          SizedBox(height: 30),
-          if (coinsVisible)
-            SizedBox(
-              height: 300,
-              width: double.infinity,
-              child: Stack(
-                children: List.generate(
-                  coinCount,
-                  (index) => Positioned(
-                    left: 30.0 * index,
-                    bottom: 0,
-                    child: buildCoin(index),
-                  ),
+          if (showCoins) ...List.generate(coinCount, buildCoin),
+          Center(
+            child: Container(
+              key: containerKey,
+              width: 150,
+              height: 150,
+              color: Colors.black54,
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: triggerAnimation,
+                  child: Text("Collect"),
                 ),
               ),
             ),
+          ),
         ],
       ),
     );
